9fee40f488bf170a0f71eba40d6be94751fa294b
vincesesto <vince_sesto@mail.com>
2021-06-10 14:17:02 +1200

    Update app.py

--- article_15/app.py
+++ article_15/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 
+app = Flask(__name__)
+
 app.secret_key = '\x98)\x861hX[\xf1\xc5Z\xd5SSe@\xe7[K\xfe\xb5\xc7~\xb0\x99\x1e\x8f\xfem\xa6F\xc1&'
 
 @app.route('/')
