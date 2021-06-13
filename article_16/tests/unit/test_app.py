f030608473d26660686b5eb8c88ee989e8ca9eca
vincesesto <vince_sesto@mail.com>
2021-06-14 11:08:28 +1200

    Create test_app.py

---
+++ article_16/tests/unit/test_app.py
@@ -0,0 +1,17 @@
+"""
+This file (test_app.py) contains the unit tests for the app.py file.
+"""
+from app import app
+
+
+def test_index_page():
+    """
+    GIVEN a Flask application
+    WHEN the '/' page is requested (GET)
+    THEN check the response is valid
+    """
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert b'Flasking App' in response.data
+        assert b'Welcome to the Flasking App!' in response.data
